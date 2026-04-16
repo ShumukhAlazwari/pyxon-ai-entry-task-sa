0dfc1c34e92d32e8a212ff3977d6cd6699168149
shumoukh alaz <shumoukh.n@gmail.com>
2026-04-16 04:40:08 +0300

    fix frontend api url

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
-
+const API_BASE = "https://pyxon-ai-entry-task-sa-production.up.railway.app";
 const fileInput = document.getElementById("fileInput");
 const uploadBtn = document.getElementById("uploadBtn");
 const uploadStatus = document.getElementById("uploadStatus");
